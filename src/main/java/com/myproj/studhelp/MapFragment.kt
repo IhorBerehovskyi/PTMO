f06e51375da2f26706be49b35c0c97b59a881e77
IhorBerehovskyi <ihor.berehovskyi.ki.2020@lpnu.ua>
2023-12-11 10:15:39 +0200

    Added simple google map

--- src/main/java/com/myproj/studhelp/MapFragment.kt
+++ src/main/java/com/myproj/studhelp/MapFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.myproj.studhelp.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
+
+    private lateinit var binding : FragmentMapBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        binding = FragmentMapBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding
     }
 
     companion object {
